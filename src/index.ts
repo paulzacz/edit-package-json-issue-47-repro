556b3c3f531e128ce598b9ee90a6caab9943dbba
Roy Revelt <roy@codsen.com>
2022-06-27 08:10:58 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { set } from "edit-package-json";
+
+export const tryme = () => set(`{
+  "name": "test",
+  "dependencies": {
+    "ranges-apply": "^2.0.0",
+    "string-left-right": "^2.3.30"
+  },
+  "devDependencies": {}
+}`,
+    "dependencies.ranges-apply",
+    "^3.2.2"
+)
